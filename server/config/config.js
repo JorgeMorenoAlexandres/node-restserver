a8a1e4f6a4dc2a7e7f83887a6ee86d5bb5a66f56
George <jorgemorenoalexandres@gmail.com>
2019-07-01 22:26:01 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://georgeferdinand:PEuRNBp8XkLvVEWE@cluster0-eykxo.mongodb.net/cafe';
+    urlDB = MONGO_URI;
 }
 process.env.URLDB = urlDB;
