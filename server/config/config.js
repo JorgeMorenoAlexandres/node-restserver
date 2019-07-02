3fcd52e6b9ff6fae2ce12f572930b4ae63f6db36
George <jorgemorenoalexandres@gmail.com>
2019-07-01 21:54:24 -0500

    REST server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,24 @@
+/***************
+ * Puerto
+ **************/
+
 process.env.PORT = process.env.PORT || 3000;
+
+/***************
+ * Ambiente
+ **************/
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/***************
+ * Base de datos
+ **************/
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://georgeferdinand:PEuRNBp8XkLvVEWE@cluster0-eykxo.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -2,44 +2,25 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+// parse application/json convierte el body del request en un json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('getUsuaraio')
-})
-app.post('/usuario', function(req, res) {
+app.use(require('./controler/usuario'));
 
-    let body = req.body;
-    if (body.nombre == undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
+//Conexión a la base de mongodb
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Conectado a la base de datos');
+    });
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuaraio')
-})
-
+//validación de puerto local o producción
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto', process.env.PORT);
 })
